ed7d6cb33c212c3948f3203f78f3f613cbb8bdc8
likunlei <likunlei@lanlingdai.net>
2019-02-19 18:49:30 +0800

    load tree when main window show

--- src/main/java/com/rick/redisbox/RedisBoxApplication.java
+++ src/main/java/com/rick/redisbox/RedisBoxApplication.java
@@ -1,18 +1,28 @@
 package com.rick.redisbox;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.Objects;
 
 public class RedisBoxApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/main.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxml/main.fxml")));
         primaryStage.setTitle("Redis Box");
         primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.setOnShown(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+
+            }
+        });
         primaryStage.show();
     }
 
