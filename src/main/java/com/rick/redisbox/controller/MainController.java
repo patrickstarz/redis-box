eff4e4da22e559969a983cc9edac253a73332fd1
likunlei <likunlei@lanlingdai.net>
2019-02-25 18:46:35 +0800

    update

--- src/main/java/com/rick/redisbox/controller/MainController.java
+++ src/main/java/com/rick/redisbox/controller/MainController.java
@@ -3,24 +3,22 @@ package com.rick.redisbox.controller;
 import com.rick.redisbox.connection.Connection;
 import com.rick.redisbox.connection.ConnectionManager;
 import com.rick.redisbox.connection.FileConnectionManager;
-import com.rick.redisbox.jedis.JedisManager;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.beans.binding.Bindings;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuItem;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,7 +30,11 @@ public class MainController implements EventHandler {
     @FXML
     private MenuItem menu_new_connection;
     @FXML
-    private ListView left_list;
+    private Menu myConnections;
+    @FXML
+    private VBox leftVBox;
+    @FXML
+    private TabPane connectionTabPanel;
 
     @FXML
     protected void onNewConnection() throws IOException {
@@ -49,33 +51,52 @@ public class MainController implements EventHandler {
         ConnectionManager manager = new FileConnectionManager();
         connections = manager.getAll();
 
-        ObservableList list = FXCollections.observableArrayList(connections);
-        left_list.setItems(list);
+        for (Connection connection : connections) {
+            MenuItem item = new MenuItem(connection.getConnName());
+            myConnections.getItems().add(item);
+
+//            Tab tab = new Tab(connection.getConnName());
+//            BorderPane borderPane = new BorderPane();
+
+//            VBox vBox = new VBox();
+//            vBox.setPrefWidth(200.0);
+//            borderPane.setLeft(vBox);
+//
+//            borderPane.setCenter(new ScrollPane());
+//            tab.setContent(borderPane);
+//            connectionTabPanel.getTabs().add(tab);
+        }
 
         //添加双击事件
-        left_list.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
-                    int index = left_list.getSelectionModel().getSelectedIndex();
-
-                    Connection connection = (Connection) left_list.getItems().get(index);
-                    Jedis jedis = jedisMap.get(connection.getId());
-                    if (jedis == null) {
-                        jedis = JedisManager.connect(connection);
-                        if (jedis != null) {
-                            jedisMap.put(connection.getId(), jedis);
-                            //TODO 展开tree
-                        }
-                    } else {
-                        //TODO 展开tree
-                    }
-
-                    //加载tree
-                    left_list.getSelectionModel().getSelectedItem();
-                }
-            }
-        });
+//        left_list.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent event) {
+//                if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
+////                    int index = left_list.getSelectionModel().getSelectedIndex();
+////                    left_list.getSelectionModel().getSelectedItem();
+//
+//                    Connection connection = (Connection) left_list.getItems().get(index);
+//                    Jedis jedis = jedisMap.get(connection.getId());
+//                    if (jedis == null) {
+//                        jedis = JedisManager.connect(connection);
+//                        if (jedis != null) {
+//                            jedisMap.put(connection.getId(), jedis);
+//                            //TODO 展开tree
+//                            TreeView treeView = new TreeView();
+//
+//                            TreeItem item = new TreeItem("root");
+//                            treeView.setRoot(item);
+//
+//                        }
+//                    } else {
+//                        //TODO 展开tree
+//                    }
+//
+//                    //加载tree
+//                    left_list.getSelectionModel().getSelectedItem();
+//                }
+//            }
+//        });
     }
 
     @Override
