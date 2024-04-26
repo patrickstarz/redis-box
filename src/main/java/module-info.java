959fdaf44639ed650210714980b1c9f309af71e5
李坤磊 <kunlei.li@hrtps.com>
2024-04-26 18:52:05 +0800

    modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module redis.box {
+    exports com.github.redisbox;
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    requires commons.io;
+    requires fastjson;
+    requires org.apache.commons.lang3;
+    requires jedis;
+}
